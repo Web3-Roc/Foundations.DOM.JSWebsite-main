0d3b6950f5547b5592d21e3bdd761d121c2ad304
Lai Saechao-Bazemore <saech@Lais-MacBook-Pro.local>
2022-12-13 17:33:08 -0500

    added h1

---
+++ index.js
@@ -0,0 +1,7 @@
+const section = document.querySelector('section');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Math Study Cards!';
+h1.style.background = 'grey';
+h1.style.textAlign = 'center';
+section.append(h1);
